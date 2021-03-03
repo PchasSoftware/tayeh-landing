754370b8f262f14264cc9d69cd38fb3edd878bff
Saeed <saeed.salehi1996@gmail.com>
2021-03-03 15:35:58 +0330

    combination

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'tayeh-ui/dist/index.min.css';
-import '@/assets/styles/global.scss';
-import TayehUi from 'tayeh-ui';
-Vue.config.productionTip = false
+import Vue from "vue";
+import Landing from "./Landing.vue";
+// import router from './router'
+import "tayeh-ui/dist/index.min.css";
+import "@/assets/styles/global.scss";
+import TayehUi from "tayeh-ui";
+Vue.config.productionTip = false;
 
 Vue.use(TayehUi);
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(Landing),
+}).$mount("#app");
